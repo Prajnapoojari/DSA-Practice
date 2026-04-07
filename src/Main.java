40492cff152e8aeca3d01e735dd3c4cd2d62e9f8
Prajnapoojari <prajnapoojari098@gmail.com>
2026-04-07 12:54:02 +0530

    fixed merge conflict

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> acd2b23 (initial commit)
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
